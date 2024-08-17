6f8e42cf8859aced31b13a0fe5dbd6804bd7ed10
steveAzo -Git <stephenazongo77@gmail.com>
2024-08-17 13:35:23 +0000

    minor update.

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -1,5 +1,6 @@
 export const fetchImageFromUri = async (uri) => {
     const response = await fetch(uri);
     const blob = await response.blob();
+    console.log("image blob...", blob)
     return blob;
   };
