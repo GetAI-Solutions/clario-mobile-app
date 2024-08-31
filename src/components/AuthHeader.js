81558b8ff4bbcf47c9cfe5ae3689ab8c74965126
issmuth <ismael.mdev@gmail.com>
2024-09-01 00:29:03 +0300

    header theme fix

--- src/components/AuthHeader.js
+++ src/components/AuthHeader.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import { View, TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { useTheme } from '../context/ThemeContext'
 
 const Header = ({ navigation }) => {
+  const { theme } = useTheme();
   const styles = StyleSheet.create({
     header: {
       flexDirection: 'row',
       justifyContent: 'flex-start',
       borderBottomWidth: 1,
-      borderBottomColor: '#000',
+      borderBottomColor: theme == 'dark' ? 'white' : 'black',
       paddingBottom: 8,
       marginBottom: 16,
       marginTop: 25,
-      backgroundColor: '#F0F0F0',
+      backgroundColor: 'transparent',
     },
   });
 
   return (
     <View style={styles.header}>
       <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Icon name="arrow-back" size={24} color="#000" />
+        <Icon name="arrow-back" size={24} color={theme == 'dark' ? 'white' : 'black'} />
       </TouchableOpacity>
     </View>
   );
